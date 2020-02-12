1103fa04c4108c66ab37e204448a8c003c1d4688
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-12 19:58:13 -0300

    Create first program

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+
+        static void Main() {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+        
+    }
+}
