766f2e9c47e469031549195dff7a9ab07c48b149
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-13 00:13:54 -0300

    Learn about Interpolation

---
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Interpolacao {
+        public static void Executar() {
+            string nome = "MacBook";
+            string marca = "Apple";
+            double preco = 6000.00;
+
+            Console.WriteLine("O " + nome 
+                              + " da marca " 
+                              + marca 
+                              + " custa " 
+                              + preco + ".");
+            Console.WriteLine("O {0} da marca {1} custa {2}.", 
+                              nome, marca, preco);
+            Console.WriteLine($"A marca {marca} é cara");
+            Console.WriteLine($"1 + 1 = {1 + 1}!");
+        }
+    }
+}
