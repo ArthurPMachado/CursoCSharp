9f8a0cc54a15ee782d4e5525c721081b66414995
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-12 23:53:38 -0300

    Learn variable types

--- CursoCSharp/Fundamentos/Comentarios.cs
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCSharp.Fundamentos {
+    /// <summary>
+    /// 
+    /// </summary>
     class Comentarios {
         public static void Executar() {
             // Cuidado com os comentários desnecessários
