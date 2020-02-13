fa2db3cff1c772c975403042b2731ba831cdf081
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-13 18:39:52 -0300

    Learn about using . to access methods and ? before the .

---
+++ CursoCSharp/Fundamentos/NotacaoPonto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class NotacaoPonto {
+        public static void Executar() {
+            var saudacao = "ola".ToUpper()?
+                                .Insert(3, " world!")?
+                                .Replace("world!", "mundo!");
+            Console.WriteLine(saudacao);
+            Console.WriteLine("Teste".Length);
+
+            string valorImportante = null;
+            Console.WriteLine(valorImportante?.Length);
+        }
+    }
+}
