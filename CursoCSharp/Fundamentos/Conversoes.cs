828a6bf1de5f7ee1926c120e17d385bd2cce7ae8
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-13 21:19:58 -0300

    Learn about conversions

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Conversoes {
+        public static void Executar() {
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            double nota = 9.7;
+            int notaTruncada = (int) nota;
+            Console.WriteLine($"Nota truncada: {notaTruncada}");
+
+            Console.Write("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+            int idadeInteiro = int.Parse(idadeString);
+            Console.WriteLine($"Idade inserida: {idadeInteiro}");
+
+            idadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine($"Resultado: {idadeInteiro}");
+
+            Console.Write("Digite o primeiro numero: ");
+            string palavra = Console.ReadLine();
+            int numero;
+            int.TryParse(palavra, out numero);
+            Console.WriteLine($"Resultado 1: {numero}");
+
+            Console.Write("Digite o segundo numero: ");
+            int.TryParse(Console.ReadLine(), out int numero2);
+            Console.WriteLine($"Resultado 1: {numero2}");
+        }
+    }
+}
