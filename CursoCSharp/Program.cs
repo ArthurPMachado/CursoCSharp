e569dbf9136bba4429d0e143f57d9cbe951245c3
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-16 13:09:47 -0300

    Resolving desafio

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -44,6 +44,7 @@ namespace CursoCSharp {
                 {"Métodos Com Retorno - Classes e Metodos", MetodosComRetorno.Executar },
                 {"Métodos Estáticos - Classes e Metodos", MetodosEstaticos.Executar },
                 {"Atributos Estáticos - Classes e Metodos", AtributosEstaticos.Executar },
+                {"Desafio Atributo - Classes e Metodos", DesafioAtributo.Executar },
             });
 
             central.SelecionarEExecutar();
