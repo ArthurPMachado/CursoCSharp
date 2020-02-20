33c8368539ec1fc86be6b641173026f2e20ebc47
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-20 18:06:58 -0300

    Learn about Sealed class

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -76,6 +76,7 @@ namespace CursoCSharp {
                 {"Polimorfismo - OO", Polimorfismo.Executar },
                 {"Abstract - OO", Abstract.Executar },
                 {"Interface - OO", Interface.Executar },
+                {"Sealed - OO", Sealed.Executar },
             });
 
             central.SelecionarEExecutar();
