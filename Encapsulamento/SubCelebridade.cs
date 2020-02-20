a7fa25f34e20661186debd0f8a8cab126b722b67
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-20 17:06:23 -0300

    Learn about encapsulation

---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Encapsulamento {
+    public class SubCelebridade {
+        // Todos
+        public string InfoPublica = "Tenho um instagram!";
+
+        // Herança
+        protected string CorDoOlho = "Verde";
+
+        // Mesmo projeto (assembly)
+        internal ulong NumeroCelular = 5511912345678;
+
+        // Herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas gírias";
+
+        // Mesma classe ou Herança no mesmo projeto (C# >= 7.2)
+        private protected string SegredoFamilia = "Bla bla";
+
+        // Private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos() {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
